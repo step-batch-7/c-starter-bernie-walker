660bb423dbbf48cbb02498b261c3b60d1b2c1942
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-03-28 17:22:20 +0530

    added is_odd

---
+++ is_odd/is_odd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+unsigned char is_even(int);
+
+unsigned char is_even(int num)
+{
+  return (num & 1) == 0 ? 1 : 0;
+}
+
+int main(void)
+{
+  int a; 
+  printf("Enter a number:\n");
+  scanf("%d", &a);
+  
+  if(is_even(a) != 1){
+    printf("%d is Odd\n", a);
+  } 
+  else
+  {
+    printf("%d is not Odd\n", a);
+  };
+  
+  return 0;
+}
